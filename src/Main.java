c2228ac056ecacf2de5086c6692cf72c4c66fa65
Samir Melo <samirp.melo@gmail.com>
2025-08-09 09:35:22 -0300

    desafio finalizado

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -16,14 +18,51 @@ public class Main {
         curso2.setCargaHoraria(40);
 
 
-        Mentoria mentoria1 = new Mentoria();
-        mentoria1.setTitulo("Mentoria para Java");
-        mentoria1.setDescricao("topicos da mentoria");
-        mentoria1.setData(LocalDate.now());
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria para Java");
+        mentoria.setDescricao("topicos da mentoria");
+        mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria1);
+        System.out.println(mentoria1);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descricao Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev dev1 = new Dev();
+        dev1.setNome("Samir");
+        dev1.inscreverBootcamp(bootcamp);
+        System.out.println("======================================");
+        System.out.printf("👨‍💻 Dev: %s%n", dev1.getNome());
+        System.out.printf("📚 Conteúdos Inscritos: %s%n", dev1.getConteudosInscritos());
+
+        dev1.progresso();
+        System.out.println("\n🔄 Progresso atualizado...");
+
+        System.out.printf("📚 Conteúdos Inscritos: %s%n", dev1.getConteudosInscritos());
+        System.out.printf("✅ Conteúdos Concluídos: %s%n", dev1.getConteudosConcluidos());
+        System.out.printf("⭐ XP Total: %.2f%n", dev1.calcularTotalXp());
+        System.out.println("======================================");
+
+        Dev dev2 = new Dev();
+        dev2.setNome("Victinho");
+        dev2.inscreverBootcamp(bootcamp);
+        System.out.println("======================================");
+        System.out.printf("👨‍💻 Dev: %s%n", dev2.getNome());
+        System.out.printf("📚 Conteúdos Inscritos: %s%n", dev2.getConteudosInscritos());
+
+        dev1.progresso();
+        System.out.println("\n🔄 Progresso atualizado...");
+
+        System.out.printf("📚 Conteúdos Inscritos: %s%n", dev2.getConteudosInscritos());
+        System.out.printf("✅ Conteúdos Concluídos: %s%n", dev2.getConteudosConcluidos());
+        System.out.printf("⭐ XP Total: %.2f%n", dev1.calcularTotalXp());
+        System.out.println("======================================");
 
 
 
